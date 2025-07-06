a9cf908205d77d34ae466a483ac098b9fb8da438
sociietyworld <shuiisociietyproductions@gmail.com>
2025-07-06 12:39:32 -0700

    Fix Supabase export and import

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,4 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mnckflpgkkytgrkylxlm.supabase.co'                  
+const supabaseUrl = 'https://mnckflpgkkytgrkylxlm.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1uY2tmbHBna2t5dGdya3lseGxtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MTgyNTg5NiwiZXhwIjoyMDY3NDAxODk2fQ.N9_MxwmxckV3UXCdKgMLQjQe1Q6ulCwh6_7vbpr43FM'
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
