66eb61d7b2e5c03ead7eebc4e8fa33665ff7c236
sociietyworld <shuiisociietyproductions@gmail.com>
2025-07-06 10:01:48 -0700

    Add minimal test homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-yellow-400 bg-black">
+      <h1 className="text-4xl font-bold">SOCIIETY Homepage is Live</h1>
+    </div>
+  )
+}
 import Link from 'next/link'
 
 export default function Home() {
